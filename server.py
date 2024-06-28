7130e77c10358571d29abe8db04cbf51b785277d
JustGithubProject <Kropivadanil@gmail.com>
2024-06-28 20:22:55 +0300

    configure project

--- server.py
+++ server.py
@@ -1,11 +1,15 @@
+"""File for creating a TCP server and processing clients."""
+
 import socket
 import threading
 
 
+# dict to store clients that connected to server
 clients = dict()
 
 
 def handle_client(client_socket: socket.socket, client_address: tuple):
+    """Processing the client in a separate thread"""
     print(f"Connected by {client_address}")
     clients[client_address] = client_socket
 
@@ -18,16 +22,20 @@ def handle_client(client_socket: socket.socket, client_address: tuple):
                 break
             print(f"{client_address}: {message}")
 
+            # Broadcast to clients
             broadcast_message(client_address, message)
 
         except Exception as ex:
             print(f'Error handling client {client_address}: {ex}')
             break
+    
+    # At the end, remove the client from the clients dict and close the connection.
     del clients[client_address]
     client_socket.close()
-    
+
 
 def broadcast_message(sender_address, message):
+    """Broadcast messages to clients except sender"""
     for client_address, client_socket in clients.items():
         if client_address != sender_address:
             try:
@@ -38,6 +46,7 @@ def broadcast_message(sender_address, message):
 
 
 def tcp_server(host: str = "127.0.0.1", port: int = 8000):
+    """start tcp-server"""
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
         server_socket.bind((host, port))
         server_socket.listen()
